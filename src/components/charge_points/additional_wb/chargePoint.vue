ea0d2509a98b341281f68e685f547ab999554e6c
ndrsnhs <andreas.neuhaus@openwb.de>
2025-11-20 09:26:28 +0100

    add cp

---
+++ src/components/charge_points/additional_wb/chargePoint.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="charge-point-additional-wb">
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="chargePoint.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-text-input
+      title="Token"
+      required
+      :model-value="chargePoint.configuration.token"
+      @update:model-value="updateConfiguration($event, 'configuration.token')"
+    />
+  </div>
+</template>
+
+<script>
+import ChargePointConfigMixin from "../ChargePointConfigMixin.vue";
+
+export default {
+  name: "ChargePointAdditionalWB",
+  mixins: [ChargePointConfigMixin],
+};
+</script>
